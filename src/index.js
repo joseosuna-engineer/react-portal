1b4aae4678066a9cb0d185ea387bf19621e086cb
Jose Osuna <jose.osuna@prottonne.org>
2017-05-28 15:02:38 -0400

    B-1 adding login.

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import { hashHistory, Route, HashRouter } from 'react-router-dom';
 
 import Home from './routes/Home';
 import Store from './routes/Store';
+import Login from './routes/Login';
 
 const app = document.getElementById('root');
 
 ReactDOM.render(
   <HashRouter history={hashHistory}>
     <div>
-      <Route exact={true} path='/' component={Home}/>
+      <Route exact={true} path='/home' component={Home}/>
       <Route exact={true} path='/store' component={Store}/>
+      <Route exact={true} path='/' component={Login}/>
     </div>
   </HashRouter>
   ,
