a817fa0f07f673931a3476e6d27435707287f131
Jose Osuna <jose.osuna@prottonne.org>
2017-06-03 10:30:06 -0400

    B-1 adding new react structure.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,91 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { hashHistory, Route, HashRouter } from 'react-router-dom';
 
-import Home from './routes/Home';
-import Store from './routes/Store';
-import Login from './routes/Login';
+class Button extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      label:props.label
+    };
+  }
+  render(){
+    return(
+      <button type="submit">{this.props.label}</button>
+    );
+  }
+}
+
+class PassInput extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      label:props.label
+    };
+  }
+    render(){
+      return(
+        <input type="password" placeholder={this.state.label} />
+      );
+    }
+}
+
+class TextInput extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      label:props.label
+    };
+  }
+    render(){
+      return(
+        <input type="text" placeholder={this.state.label} />
+      );
+    }
+}
+
+class LoginForm extends React.Component {
+  render(){
+    return(
+      <div>
+        <form>
+          <TextInput label="Correo electrónico" />
+          <PassInput label="Contraseña" />
+          <Button label="Iniciar sesión" />
+        </form>
+      </div>
+    );
+  }
+}
+
+class HeaderTitle extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      label:props.label
+    };
+  }
+  render(){
+    return(
+      <div><span>{this.state.label}</span></div>
+    );
+  }
+}
+
+class LoginPage extends React.Component {
+  render(){
+    return(
+      <div>
+        <HeaderTitle label="Inicia sesión en Prottonne"/>
+        <LoginForm />
+      </div>
+    );
+  }
+}
 
 const app = document.getElementById('root');
 
 ReactDOM.render(
-  <HashRouter history={hashHistory}>
-    <div>
-      <Route exact={true} path='/home' component={Home}/>
-      <Route exact={true} path='/store' component={Store}/>
-      <Route exact={true} path='/' component={Login}/>
-    </div>
-  </HashRouter>
+  <LoginPage />
   ,
   app
 );
