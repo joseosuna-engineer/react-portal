2550879f6cb4e7b1f8e784c330814defbb485385
Jose Osuna <joseosuna.engineer@gmail.com>
2017-06-13 18:55:42 -0400

    B-1 adding constants.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import  IndexPage  from './route';
+import {
+  AUTH_TOKEN_LOCAL_NAME
+} from './action/action-const';
+import { setAuthToken } from './action/auth-action';
+
+setAuthToken(localStorage.getItem(AUTH_TOKEN_LOCAL_NAME));
 
 const app = document.getElementById('root');
 
