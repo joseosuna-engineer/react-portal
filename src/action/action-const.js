f5718deaec565226fef0d2d5b8f24c591177ca2b
Jose Osuna <joseosuna.engineer@gmail.com>
2017-06-14 21:16:40 -0400

    B-1 refacting path.

--- src/action/action-const.js
+++ src/action/action-const.js
@@ -5,7 +5,7 @@
 
 export const AUTH_TOKEN_LOCAL_NAME = '__vrfeqFpHOe__';
 export const AUTH_HEADER = 'Authorization';
-export const LOGIN_PATH = '/login';
+export const LOGIN_PATH = 'lab/login';
 export const SET_USER_EMAIL = 'SET_USER_EMAIL';
 export const SET_USER_PASSWORD = 'SET_USER_PASSWORD';
 export const SET_USER_AUTH = 'SET_USER_AUTH';
