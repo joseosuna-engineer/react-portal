ae2794d4830bb0733848dc975f826a22c1ea5ec6
Jose Osuna <jose.osuna@prottonne.org>
2017-06-14 21:16:40 -0400

    B-1 refacting path.

--- src/route/HomePage.js
+++ src/route/HomePage.js
@@ -8,23 +8,11 @@ import { Link } from 'react-router-dom';
 
 export default class HomePage extends Component {
   render(){
-
-    if(this.props.user.isAuth){
       return(
         <div>
           <h1>Welcome {this.props.user.email}!</h1>
-          <h4>You are authenticated: yes!</h4>
-          <Link to="/">Logout</Link>
-        </div>
-      );
-    }else{
-      return(
-        <div>
-          <h1>Welcome {this.props.user.email}!</h1>
-          <h4>You are authenticated: no!</h4>
           <Link to="/login">Logout</Link>
         </div>
       );
-    }
   }
 }
