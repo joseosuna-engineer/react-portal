fbc7c5e44049c07dedb00a7b9f15ee6b599bcba1
Jose Osuna <joseosuna.engineer@gmail.com>
2017-07-01 14:29:02 -0400

    B-1 simulate several calls to backend.

--- src/component/MySettings.js
+++ src/component/MySettings.js
@@ -6,11 +6,18 @@
 import React, { Component } from 'react';
 
 export default class MySettings extends Component {
-
+  componentDidMount(){
+    if(this.props.user.isAuth){
+      this.props.getProfile(this.props.user);
+    }
+  }
   render(){
     return(
       <div>
             <h1>Configuración</h1>
+            <h2>Perfil</h2>
+            <h3>Nombre: {this.props.user.firstName}</h3>
+            <h3>Apellido: {this.props.user.lastName}</h3>
       </div>
     );
   }
