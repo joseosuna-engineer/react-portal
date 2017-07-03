b3ea9e8ed366704df0d533b08a29d9ed387d782f
Jose Osuna <joseosuna.engineer@gmail.com>
2017-07-02 22:24:58 -0400

    B-1 adding refresh management.

--- src/route/index.js
+++ src/route/index.js
@@ -13,8 +13,18 @@ require('../style/signin.css');
 import store from '../reducer';
 require('../style/navbar.css');
 import AppPage from './AppPage';
+import { setAuthToken, setUserAuth } from '../action/user-action';
+import { AUTH_TOKEN_LOCAL_NAME } from '../action/action-const';
+import jwtDecode from 'jwt-decode';
 
 export default class IndexPage extends Component {
+  componentWillMount() {
+    let token = localStorage.getItem(AUTH_TOKEN_LOCAL_NAME);
+    if(token){
+      setAuthToken(token);
+      store.dispatch(setUserAuth(jwtDecode(token)));
+    }
+  }
   render(){
     return(
       <Provider store={store}>
