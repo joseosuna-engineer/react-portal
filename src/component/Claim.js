bea8038d75573590efcc77781423804e9da63846
Jose Osuna <jose.osuna@prottonne.org>
2017-07-07 19:35:13 -0400

    B-1 adding claim path.

---
+++ src/component/Claim.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * Copyright 2017 Prottonne
+ */
+
+import React, { Component } from 'react';
+
+export default class Claim extends Component {
+  render(){
+    return(
+      <div>
+        Reclamos
+      </div>
+    );
+  }
+}
